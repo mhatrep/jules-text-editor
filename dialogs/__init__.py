09380392ebf67265305f9b86a920689499ea72ed
Prashant Mhatre <mhatrep@gmail.com>
2025-07-10 22:08:20 -0400

    Delete dialogs directory

--- dialogs/__init__.py
+++
@@ -1,3 +0,0 @@
-# This file makes the 'dialogs' directory a Python package.
-# It can be left empty or used to auto-import dialog classes if desired.
-# For now, we'll keep it simple and import explicitly in editor.py.
