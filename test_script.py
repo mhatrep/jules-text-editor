eb2fdbd3b4d698ca002c27720c257569a840eae0
Prashant Mhatre <mhatrep@gmail.com>
2025-07-14 22:22:28 -0400

    Delete test_script.py

--- test_script.py
+++
@@ -1,9 +0,0 @@
-import sys
-import time
-print("Hello from Python!")
-print(f"Arguments: {sys.argv[1:]}")
-for i in range(5):
-    print(f"Python count: {i}")
-    time.sleep(0.2)
-print("Python script finished.")
-sys.stderr.write("This is a Python stderr message.\n")
